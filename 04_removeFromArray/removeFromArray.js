4ce5a9339e03b7795971d30d96ad931d44756699
ubman0 <117761816+ubman0@users.noreply.github.com>
2022-12-03 16:06:38 -0700

    Add solution which doesn't work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(args, vallue) {
+    const array = args[0];
+    return array.filter(val => !array.includes(val))
 };
 
 // Do not edit below this line
