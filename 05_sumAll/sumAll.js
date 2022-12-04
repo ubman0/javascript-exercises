8f0f29562bcf22993c4fac4e0e41ce24c16014d2
ubman0 <117761816+ubman0@users.noreply.github.com>
2022-12-04 13:17:02 -0700

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(lower, upper) {
+    if ((lower || upper) < 0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(lower) || !Number.isInteger(upper)){
+        return "ERROR";
+    }
+    if (lower > upper){
+        let temp = upper;
+        upper = lower;
+        lower = temp;
+    }
+    let total = 0;
+    for (let i = lower; i < upper + 1; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
