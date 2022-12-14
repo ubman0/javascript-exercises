b963cbad80eb688f64ec0b6111f601a69468600b
ubman0 <117761816+ubman0@users.noreply.github.com>
2022-12-14 13:17:31 -0700

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    total = 1;
+    beforeTotal = 0;
+    for (let i = 1; i < number; i++){
+        const temp = total;
+        total = beforeTotal + total;
+        beforeTotal = temp;
+    }
+    return total;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
