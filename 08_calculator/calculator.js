6e6ebb4f702433a40a083ef697f960da8da1a504
ubman0 <117761816+ubman0@users.noreply.github.com>
2022-12-13 18:34:47 -0700

    Completed factorial()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -22,12 +22,15 @@ const multiply = function(numbers) {
 
 const power = function(x, y) {
   total = 1;
-	for (let i = 0; i < y; i++) {total *= x};
+	for (let i = 0; i < y; i++) total *= x;
   return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) return 1;
+	let total = 1;
+  for (let i = x; i > 0; i--) total *= i;
+  return total;
 };
 
 // Do not edit below this line
