6e066ac3d5d397d6fdc8b3dcfe981e4a13997ebc
ubman0 <117761816+ubman0@users.noreply.github.com>
2022-12-14 14:11:49 -0700

    Started findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    currentYear = (new Date().getFullYear());
+    const sortedByAge = people.sort((a, b) => (a.yearOfBirth - a.yearOfDeath) > (b.yearOfBirth - b.yearOfDeath) ? 1:-1);
+    if (sortedByAge[0] && !(sortedByAge[0].yearOfDeath)) return sortedByAge[0];
 };
 
 // Do not edit below this line
